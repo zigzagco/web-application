1a5db165838eb322e601f5b502dac245161b378e
gleb <g1397209@gmail.com>
2022-04-13 05:09:07 +0300

    -v2
    Signed-off-by: gleb <g1397209@gmail.com>

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   productionBrowserSourceMaps: true,
   env: {
     //API_URL:'http://192.168.5.125:8000/api'
-    API_URL: 'http://localhost:8000/api'
+    API_URL: 'http://localhost:80/api'
   },
   //i18n: {
  //   locales: ['ru', 'en'],
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 80
 app.use(cors())
 app.use(express.json({extended:true}))
 app.use('/api/post',require('./routes/post.routes'))
