2b5ce14d5cf55967482322c1f05582a6b24c69dd
gleb <g1397209@gmail.com>
2022-04-13 06:52:59 +0300

    -v2
    Signed-off-by: gleb <g1397209@gmail.com>

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 module.exports = {
   productionBrowserSourceMaps: true,
   env: {
-    //API_URL:'http://192.168.5.125:8000/api'
-    API_URL: 'http://localhost:8080/api'
+    API_URL:'http://192.168.5.125:8080/api'
+    //API_URL: 'http://localhost:8080/api'
   },
   //i18n: {
  //   locales: ['ru', 'en'],
