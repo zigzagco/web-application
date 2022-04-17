189ab71f90465725667a59ae27b0a8796490f02a
gleb <g1397209@gmail.com>
2022-04-17 23:28:23 +0300

    -v2
    Signed-off-by: gleb <g1397209@gmail.com>

--- client/components/Scrolable.js
+++ client/components/Scrolable.js
@@ -4,24 +4,24 @@ import ScrollContainer from "react-indiana-drag-scroll";
 
 const Scrolable = props => {
     const headers = [
-        {n:"1",title:"Политика",link:"politika"},
-        {n:"2",title:"В мире",link:"v_mire"},
-        {n:"3",title:"Экономика",link:"ekonomika"},
-        {n:"4",title:"Общество",link:"ekonomika"},
-        {n:"5",title:"Политика",link:"politika"},
-        {n:"6",title:"В мире",link:"v_mire"},
-        {n:"7",title:"Экономика",link:"ekonomika"},
-        {n:"8",title:"Общество",link:"ekonomika"},
-        {n:"9",title:"Политика",link:"politika"},
-        {n:"10",title:"В мире",link:"v_mire"},
+        {title:"Политика",link:"politika"},
+        {title:"В мире",link:"v_mire"},
+        {title:"Экономика",link:"ekonomika"},
+        {title:"Общество",link:"ekonomika"},
+        {title:"Политика",link:"politika"},
+        {title:"В мире",link:"v_mire"},
+        {title:"Экономика",link:"ekonomika"},
+        {title:"Общество",link:"ekonomika"},
+        {title:"Политика",link:"politika"},
+        {title:"В мире",link:"v_mire"},
 
     ]
     return (
         <div className="container">
             <div className="container_head">
                 <ScrollContainer className="items" horizontal={true} hideScrollbars={true}>
-                    {headers.map(el => (
-                        <div key={el.n} className="cattext">
+                    {headers.map((el,index) => (
+                        <div key={index} className="cattext">
                             <div className="ctext">
                                 <Link href={"/[dir]"} as={"/"+el.link} passHref>
                                     <span className="catname">{el.title}</span>
